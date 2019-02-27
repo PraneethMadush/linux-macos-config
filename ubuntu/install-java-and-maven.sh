cd8a263acd6c31f30ee9892a76c9ff9da4bc1575
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2019-02-28 00:07:37 +0530

    Update install-java-and-maven.sh
    Fix the issue of "${PATH}" resolving into the actual $PATH inside the script.

--- ubuntu/install-java-and-maven.sh
+++ ubuntu/install-java-and-maven.sh
@@ -7,7 +7,7 @@ cd /opt/ &&
 wget https://www-eu.apache.org/dist/maven/maven-3/3.6.0/binaries/apache-maven-3.6.0-bin.tar.gz
 sudo tar -xvzf apache-maven-3.6.0-bin.tar.gz &&
 sudo mv apache-maven-3.6.0-bin.tar.gz maven &&
-printf "export M2_HOME=/opt/maven \n export PATH=${M2_HOME}/bin:${PATH}" > /etc/profile.d/mavenenv.sh && 
+printf "export M2_HOME=/opt/maven \n export PATH=\${M2_HOME}/bin:\${PATH}" > /etc/profile.d/mavenenv.sh && 
 sudo chmod +x /etc/profile.d/mavenenv.sh &&
 source /etc/profile.d/mavenenv.sh
 
